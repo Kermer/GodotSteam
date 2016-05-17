9c3ea90b4d967210b846fdc16e616ceb29d8cd4e
Kermer <Kermer@users.noreply.github.com>
2016-05-18 00:29:42 +0200

    API&Organisation Rework
    Merge pull request #6 from Kermer/beta

--- godotsteam/register_types.cpp
+++ godotsteam/register_types.cpp
@@ -5,32 +5,29 @@
 #include "object_type_db.h"
 
 #include "godotsteam.h"
-#include "gsteamgameserver.h"
+#include "godotsteam_uncommon.h"
 
 #include "globals.h"
 
 static Steam* SteamPtr = NULL;
-static _SteamGameServer* SteamGServer = NULL;
+static SteamUC* SteamUCPtr = NULL;
 
 
 void register_godotsteam_types()
 {
 	SteamPtr = memnew(Steam);
-	SteamGServer = memnew(_SteamGameServer);
+	SteamUCPtr = memnew(SteamUC);
 	
 	Globals::get_singleton()->add_singleton(Globals::Singleton("Steam",Steam::get_singleton()));
-	Globals::get_singleton()->add_singleton(Globals::Singleton("SteamGameServer",_SteamGameServer::get_singleton()));
+	Globals::get_singleton()->add_singleton(Globals::Singleton("SteamUC",SteamUC::get_singleton()));
 	
 	
 	ObjectTypeDB::register_virtual_type<Steam>();
-	ObjectTypeDB::register_virtual_type<_SteamGameServer>();
-	//ObjectTypeDB::register_type<SteamID>();
-	ObjectTypeDB::register_virtual_type<_SteamUser>();
-	ObjectTypeDB::register_virtual_type<_SteamGroup>();
+	ObjectTypeDB::register_virtual_type<SteamUC>();
 }
 
 void unregister_godotsteam_types()
 {
 	memdelete(SteamPtr);
-	memdelete(SteamGServer);
+	memdelete(SteamUCPtr);
 }
